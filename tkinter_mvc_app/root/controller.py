4aadafec6a97cf2846f4b42a2291f93167e4b669
Edgar CORTES <edgar.cor.hdz@gmail.com>
2024-01-22 15:37:59 +0100

    Implemented save button for book information
    Implemented first lines of code to get information of title_entry and already_read_checkbutton when press save button:
    - Updated root/controller.py
    - Modified widgets_creation function to implement a return of widgets
      created
    - Modified attribut author of Book model
    - Updated BookFrame class at views/frames.py
    - Implemented SaveBookController class at save_book.py

--- tkinter_mvc_app/root/controller.py
+++ tkinter_mvc_app/root/controller.py
@@ -1,9 +1,14 @@
+from tkinter_mvc_app.books.controllers.save_book import SaveBookController
+from tkinter_mvc_app.books.models.models import Book
+from tkinter_mvc_app.books.views.frames import BookFrame
 from tkinter_mvc_app.root.views import MainView
 
 
 class Controller:
     def __init__(self, view: MainView):
         self.view = view
+        # self.model = model
+        self.save_book_controller = SaveBookController(view)
 
     def run(self):
         self.view.run_mainloop()
